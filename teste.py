72cb0123df75fac027e51259ec512990d9391de1
João Henrique Ferreira <jaycops@gmail.com>
2017-12-21 11:57:25 -0200

    desmembramento funca fill/apont

--- teste.py
+++ teste.py
@@ -1,16 +1,5 @@
 import datetime
 
-def cabec(val1,val2):#hora de início do colaborador, hh/mm
-        if val1<10:
-            val1="0"+str(val1)+":"
-        else:
-            val1=str(val1)+":"
-        if val2<10:
-            val2="0"+str(val2)
-        else:
-            val2=str(val2)
-        return val1, val2
-
 def ApEst(val1,val2):#Apontamento estático
     print("\n\n","Tempo da atividade em horas")
     dh=int(input()) #duração horas
@@ -38,9 +27,27 @@ def ApDin():
     dh,dm=now.hour, now.minute
     return dh, dm
 
+#funcao para casting
+def cabec(val1,val2):#hora de início do colaborador, hh/mm
+        if val1<10:
+            val1="0"+str(val1)+":"
+        else:
+            val1=str(val1)+":"
+        if val2<10:
+            val2="0"+str(val2)
+        else:
+            val2=str(val2)
+        return val1, val2
 
-def fill(f,val1,val2): #hr, min
-    op=0
+def fill(f,hin,miin,hfin,mfin,av):#arquivo, hh/mm iniciais, hh/mm finais, comentarios
+    if hin<=12 and hfin>=12 and mfin>0:
+        exit()
+    hfin,mfin=cabec(hfin,mfin)
+    hin,miin=cabec(hin,miin)
+    f.write(hin+miin+" - "+hfin+mfin+" -- "+av+"\n")
+
+def Apont(f,val1,val2): #arquivo,hr, min --realizado apontamento
+    op=0#definido como 0 para entrada no loop
     #Enquanto não for preenchido com valor certo, loop
     while op!=1 or op!=2:
         print("\n\n|-------------------------------------|")
@@ -56,14 +63,11 @@ def fill(f,val1,val2): #hr, min
             break
     print("\n\n","Digite uma breve descrição da atividade:")
     av=input(str()) #atividade
-    dh,dm=cabec(dh,dm)
-    val1,val2=cabec(val1,val2)
-    f.write(val1+val2+" - "+dh+dm+" -- "+av+"\n")
+    fill(f,val1,val2,dh,dm,av)#chamada de função para preenchimento no arquivo
 
 def read(val1, val2):# nome do arquivo, valor inicial da string, valor final da string
     name=MontaTitulo()
     f=open(name,"r")
-
     value=int(f.readlines()[-1][val1:val2] ) #valor ultima hora
     f.close
     return value
@@ -108,12 +112,12 @@ def main():
         hin=int(input())
         print("\n\n","Digite minutos de inicio do trabalho:")
         minn=int(input())
-        fill(f,hin,minn) #Hora de início padrão//(hora,minutos)
+        Apont(f,hin,minn) #Hora de início padrão//(hora,minutos)
     except FileExistsError as x:
         vh=read(8,10) #posição da ultima hora
         vm=read(11,13)#posição do ultimo minuto
         f=open(name,"a")
-        fill(f,vh,vm)
+        Apont(f,vh,vm)
     f.close
 
 def menu():
@@ -136,4 +140,3 @@ def menu():
         exit()
 
 menu()
-#teste
